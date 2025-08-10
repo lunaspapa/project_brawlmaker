a5ba432d00f3a04ac152bb9b0c728e29424b9be2
Luis <moondad2022@gmail.com>
2025-08-10 11:24:01 -0700

    changed file structure

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,9 +1,8 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
+from db import db
 
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   email = db.Column(db.String, unique=True, email=True, nullable=False)
   username = db.Column(db.String, unique=True, nullable=False)
   password = db.Column(db.String, nullable=False)
+  avatar = db.Column(db.String)
